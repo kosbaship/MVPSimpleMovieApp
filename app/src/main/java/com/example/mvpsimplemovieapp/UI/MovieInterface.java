5156c12e5e9d3bfd435cf592faf1fd9331f7ce17
kosba <kosbaship>
2020-05-18 12:46:04 +0200

    MVVM

--- app/src/main/java/com/example/mvpsimplemovieapp/UI/MovieInterface.java
+++
@@ -1,7 +0,0 @@
-package com.example.mvpsimplemovieapp.UI;
-
-public interface MovieInterface {
-//     ابني الانترفيس ويكون فيه الداله وهي حلقه الوصل بين الريزينتر
-//     والفيو او بمعني تاني البراميتارز بتاعتها هيا الداتا اللي هتسخدمها مباشر للعرض
-    public void onGetMovieDetails(String movieName, String movieDate, String movieDispcription);
-}
